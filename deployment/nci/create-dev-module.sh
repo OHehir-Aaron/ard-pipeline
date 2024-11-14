08890bc9ac09ef8e3dd42f76dcd497ac4832fa5b
Imam Tashdid ul Alam <imam.alam@gmail.com>
2024-11-14 19:50:22 +1100

    turn lambertian on (as this is a debug branch)

--- deployment/nci/create-dev-module.sh
+++ deployment/nci/create-dev-module.sh
@@ -7,4 +7,5 @@ cd "$this_dir"
 
 # Create in out dev module dir
 export module_dir="/g/data/up71/modules"
+export ard_product_array=${ard_product_array:="[\"LAMBERTIAN\", \"NBART\", \"NBAR\"]"}
 ./create-module.sh "${@}"
